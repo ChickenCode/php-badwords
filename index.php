166fe1d15b03eca5a1f5338f6bedaf55f519b441
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-05-31 05:39:16 -0400

    a

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
     $badword = $_GET["badword"];
     $string = "Non dire mai la parola " . $badword;
+    echo $string;
+
     $replaced = str_replace($badword, "***", $string);
+
     echo $replaced;
 
+    echo strlen($string);
 ?>
